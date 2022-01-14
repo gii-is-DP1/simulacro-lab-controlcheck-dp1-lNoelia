9a56598901006c28a41c3dfde5173649298b7943
lNoelia <noelialopezduran@gmail.com>
2022-01-14 13:08:08 +0100

    Examen simulacro

--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,5 +1,45 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    private ProductService prService;
     
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "products/createOrUpdateProductForm";
+
+
+    @GetMapping("/create")
+	public String initCreationForm( ModelMap model) {
+		Product product = new Product();
+        model.addAttribute("product", product);
+        model.addAttribute("productType", prService.findAllProductTypes());
+		return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+	}
+
+    @PostMapping(path="/create")
+	public String processCreationForm(@Valid Product product, BindingResult result, ModelMap model) {	
+        String view = "welcome";
+		if (result.hasErrors()) {
+            model.addAttribute("product", product);
+            model.addAttribute("productType", prService.findAllProductTypes());
+			return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
+		}else {
+            prService.save(product);
+            model.addAttribute("message","Product succesfully created");
+        }
+        return view;
+    }
 }
+
--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,12 +3,17 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
-
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product,Integer>{
+    @Query("SELECT p from Product p WHERE p.price<?1")
+    List<Product> findByPriceLessThan(Double cost);
+    @Query("SELECT p from ProductType p WHERE p.name=?1")
+    ProductType getProductType(String name);
     List<Product> findAll();
+    @Query("SELECT p from ProductType p")
     List<ProductType> findAllProductTypes();
     Optional<Product> findById(int id);
     Product findByName(String name);
